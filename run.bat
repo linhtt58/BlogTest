7389d594f20054bb7379fb7c42b6c36f196888c1
Trần Tuấn Linh <linhtt_58@vnu.edu.vn>
2019-05-23 22:46:16 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,10 @@
+set FN_AUTH_REDIRECT_URI=http://localhost:5000/google/auth
+set FN_BASE_URI=http://localhost:5000
+set FN_CLIENT_ID=899728043488-88v6vntt3pronvdueidbunbfmgqudprt.apps.googleusercontent.com
+set FN_CLIENT_SECRET=ETVs9_s-qc5k6gVBKEV4dgp6
+
+set FLASK_APP=app.py
+set FLASK_DEBUG=1
+set FN_FLASK_SECRET_KEY=SOMETHING RANDOM AND SECRET
+
+python -m flask run
